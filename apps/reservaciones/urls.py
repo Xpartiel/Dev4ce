06aee0717d32d09c5143637f5a46afb77e9315a0
YamilSalazar <peyasago@ciencias.unam.mx>
2026-05-26 20:02:43 -0600

    Se agrega parques y detalle de parques

--- apps/reservaciones/urls.py
+++ apps/reservaciones/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("dashboard/", views.dashboard_cliente, name="dashboard_cliente"),
     path("mapa/",views.mapa_cliente, name="mapa_cliente"),
+    path("parques/<int:parque_id>/", views.detalle_parque, name="detalle_parque"),
 ]
--- apps/reservaciones/views.py
+++ apps/reservaciones/views.py
@@ -9,3 +9,27 @@ def dashboard_cliente(request):
 @login_required
 def mapa_cliente(request):
     return render(request, "reservaciones/mapa_cliente.html")
+
+@login_required
+def detalle_parque(request, parque_id):
+
+    parque = {
+        "id": parque_id,
+        "nombre": "Santuario El Rosario",
+        "estado": "Michoacán",
+        "descripcion": "Un santuario rodeado de bosque, ideal para vivir una experiencia nocturna entre luciérnagas.",
+        "precio": 850,
+        "disponibles": 47,
+        "servicios": [
+            "Cabañas",
+            "Camping",
+            "Senderos guiados",
+            "Estacionamiento",
+            "Baños",
+            "Zona de alimentos",
+        ],
+    }
+
+    return render(request, "reservaciones/detalle_parque.html", {
+        "parque": parque
+    })
